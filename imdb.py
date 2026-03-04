82f807d62bc06ecaaf10d36fb0157a9c0c40e65c
Jotham (jotham.co) <jotham.read@gmail.com>
2026-03-04 22:52:57 +1300

    Switched to OMDB API.  Now requires ``` [plzbum_imdb] api_key = foo ``` stanza in default.cfg for sopel.

--- imdb.py
+++ imdb.py
@@ -3,15 +3,35 @@ import re
 import requests
 
 IMDB_URL_PATTERN = r'https://www\.imdb\.com/title/([a-zA-Z0-9]+)/?'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'}
+OMDB_URL = 'https://www.omdbapi.com/'
 
-def get_imdb_title(imdb_id):
-    url = 'https://www.imdb.com/title/{}/'.format(imdb_id)
-    response = requests.get(url, headers=HEADERS)
-    match = re.search(r'og:title" content="([^"]+)"', response.text)
-    if match:
-        return match.group(1).split(' | ')[0]
-    return None
+def get_omdb_info(imdb_id, api_key):
+    response = requests.get(OMDB_URL, params={'i': imdb_id, 'apikey': api_key})
+    if response.status_code != 200:
+        return None
+    data = response.json()
+    if data.get('Response') == 'False':
+        return None
+    return data
+
+def format_title(data):
+    parts = [data.get('Title', 'Unknown')]
+    year = data.get('Year')
+    if year:
+        parts[0] += ' ({})'.format(year)
+    media_type = data.get('Type', '').capitalize()
+    if media_type:
+        parts.append(media_type)
+    rating = data.get('imdbRating')
+    if rating and rating != 'N/A':
+        parts.append('IMDB: {}/10'.format(rating))
+    votes = data.get('imdbVotes')
+    if votes and votes != 'N/A':
+        parts.append('{} votes'.format(votes))
+    genre = data.get('Genre')
+    if genre and genre != 'N/A':
+        parts.append(genre)
+    return ' | '.join(parts)
 
 try:
     import sopel.module
@@ -20,20 +40,23 @@ except ImportError:
 else:
     @sopel.module.rule(r'.*' + IMDB_URL_PATTERN + r'.*')
     def f_imdb(bot, trigger):
+        api_key = bot.config.plzbum_imdb.api_key
         match = re.search(IMDB_URL_PATTERN, trigger.group(0))
         if match:
-            title = get_imdb_title(match.group(1))
-            if title:
-                bot.say(title)
+            data = get_omdb_info(match.group(1), api_key)
+            if data:
+                bot.say(format_title(data))
 
 if __name__ == '__main__':
     import sys
-    imdb_id = 'tt4359416'
-    if len(sys.argv) > 1:
-        imdb_id = sys.argv[1]
-    print('Looking up "{}"'.format(imdb_id))
-    title = get_imdb_title(imdb_id)
-    if title:
-        print(title)
+    if len(sys.argv) != 3:
+        print('Usage: imdb.py <omdb api_key> <imdb_id>')
     else:
-        print('Could not find title for {}'.format(imdb_id))
+        api_key = sys.argv[1]
+        imdb_id = sys.argv[2]
+        print('Looking up "{}"'.format(imdb_id))
+        data = get_omdb_info(imdb_id, api_key)
+        if data:
+            print(format_title(data))
+        else:
+            print('Could not find info for {}'.format(imdb_id))
